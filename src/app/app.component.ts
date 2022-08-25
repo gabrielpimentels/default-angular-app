8d95bc6286a1c68a2fb660182d81c3017cd327f0
Gabriel Santos <gabrielpimentels@live.com>
2022-08-25 14:15:35 +0100

    Add ESLint/Prettier (ng lint --fix)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'default-angular-app';
+    title = 'default-angular-app';
 }
