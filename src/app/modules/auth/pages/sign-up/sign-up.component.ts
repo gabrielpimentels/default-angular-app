4be04719b2c15f48305e030caf08d7ebc2cfb98c
Gabriel Santos <gabrielpimentels@live.com>
2022-08-25 16:33:17 +0100

    Add RouterLink/SignIn/SignUp

--- src/app/modules/auth/pages/sign-up/sign-up.component.ts
+++ src/app/modules/auth/pages/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-sign-up',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
     styleUrls: ['./sign-up.component.scss'],
 })
 export class SignUpComponent {
-    constructor() {}
+    constructor(private router: Router) {}
+
+    signUp() {
+        this.router.navigate(['sign-in']);
+    }
 }
